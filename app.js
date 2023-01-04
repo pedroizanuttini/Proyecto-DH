857de12508ed4df9b8b852caa5d749595cf0ccca
Fede440 <fmramirez12@gmail.com>
2023-01-04 08:46:29 -0300

    agregado_login

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app=express();
 const path = require('path');
 app.use('static', express.static(__dirname + '/public'));
 app.use(express.static('public'));
-
 app.listen(3000, () => {
     console.log('Servidor corriendo en el servidor 3000')
 });
-
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname + '/views/home.html'));
+});
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'));
 });
 app.get('/register',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/register.html'));
