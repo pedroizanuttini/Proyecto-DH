e8119ac29ef9226050fe4b1d1a0dcd7a57405a4c
pedroizanuttini <pedroizanuttini@gmail.com>
2023-01-29 22:24:31 +1100

    Subo archivos json y administracion completa

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,10 +1,31 @@
-const { response } = require('express');
+const { response } = require('express')
 
+
+const showProducts = (req, res=response) => {
+    res.render('./products/productslist',{});
+}
 
 const showProductsFormEdit = (req, res=response) => {
     res.render('./products/productsform',{});
 }
 
+const showProductDetail = (req, res=response) => {
+    res.render('./products/productDetail',{});
+}
+
+const createProduct = (req, res=response) => {
+    return;
+}
+
+const updateProduct = (req, res=response) => {
+    return;
+}
+
+const deleteProduct = (req, res=response) => {
+    return;
+}
+
+
 module.exports = {
-    showProductsFormEdit
+    showProductsFormEdit, showProducts, showProductDetail, createProduct, updateProduct, deleteProduct
 }
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,17 @@
 const { Router } = require('express');
-const { showProductsFormEdit } = require('../controllers/products.controller');
+const { showProductsFormEdit, showProducts, showProductDetail, createProduct, updateProduct, deleteProduct } = require('../controllers/products.controller');
 
 const router = Router();
 
+router.get('/', showProducts); //http://localhost:3000/products
 router.get('/new' , showProductsFormEdit ); //http://localhost:3000/products/new
+router.get('/:id', showProductDetail); //http://localhost:3000/products/:id
+
+router.post('/', createProduct); //http://localhost:3000/products
+router.get('/:id/edit' , showProductsFormEdit ); //http://localhost:3000/products/:id/edit
+router.put('/:id', updateProduct); //http://localhost:3000/products/:id
+router.delete('/:id', deleteProduct) //http://localhost:3000/products/:id
+
 
 // module.exports = router;
 module.exports = router;
