6b7a05785098e70e025e17032df432abfc0d708a
pedroizanuttini <pedroizanuttini@gmail.com>
2023-02-19 15:56:24 +1100

    agrego middleware a registro usuario

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const {showLogin,showRegister, createUser, login}=require('../controllers/auth.controller');
 const multer  = require('multer')
 const upload = multer({ dest: 'public/uploads' })
+const { fieldValidator } = require('../middlewares/fields-validators');
 
 const router = Router();
 
@@ -9,7 +11,12 @@ router.get('/login', showLogin)  // http://localhost:3000/auth/login
 router.get('/register', showRegister)  // http://localhost:3000/auth/register
 
 router.post('/new', upload.single('avatar'),createUser)  //http://localhost:3000/auth/new
-router.post('/login', login);
+router.post('/login',[                                   
+    check('email','el nombre del campo es obligatorio').not().isEmpty(),
+    check('email','el valor ingresado no es un email válido').isEmail(),
+    check('password','la contraseña debe tener entre 6 y 12 caracteres').isLength({ min:6, max:12 }), 
+    fieldValidator 
+],login);//http://localhost:3000/auth/login
 
 module.exports = router;
 
